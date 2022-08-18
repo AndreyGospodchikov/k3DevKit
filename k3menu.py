70e606b565ac163d32e73f39b6d3b5d9dda7ab75
AndreyGospodchikov <bstud@yandex.ru>
2022-08-18 12:12:00 +0300

    Add methods to fill menu window
    Start brilliant work to create new classes for menu

--- k3menu.py
+++ k3menu.py
@@ -4,6 +4,12 @@
 
 Методы класса:
 
+Add_Caption(caption) - Задаёт заголовок меню
+Add_Picture(picture) - Задаёт путь к файлу картинки для меню
+Align_Text(align) - 'right' или 'center' задают выравнивание текста по правому краю или по центру
+    В противном случае будет выравнивание по левому краю
+Add_Text(text) - добавляет строку пояснительного текста в окно меню
+
 Show() - Отображает меню в К3. Возвращает 1, если в меню нажали Ок или 0, если нажали Отмена или закрыли меню
 Add_ - методы для добавления в меню строчек для внесения данных
     Real - ввод числа
@@ -16,6 +22,7 @@ Add_ - методы для добавления в меню строчек для внесения данных
 Val(имя) - возвращает значение из позиции меню с заданным именем.
     Возвращает значение, введённное в меню, если меню отображалось и там нажали Ок
     Возвращает умолчания, если меню ещё не отображалось
+Val_From_Subst(имя) - возвращает значение id для позиций типа String 6 без номера подстановки
 Delbyname(имя) - удаляет из меню позицию с заданным именем
 Move(n1, n2) - Переносит n1-ю позицию в n2
 Refresh() - Заносит значения, введённые пользователем у всех позиций меню в их умолчания
@@ -26,31 +33,47 @@ import k3
 
 class k3menu:
 
-    def __init__(self, capt='', pic='', kwrd='left', text=''):
+    def __init__(self, caption='', picture='', align='left', text=''):
         """ При вызове получаем
-        capt - заголовок окна
-        pic - имя файла для картинки
-        kwrd - ключевое слово для выравнивания (left, right, center)
+        caption - заголовок окна
+        picture - имя файла для картинки
+        align - выравнивание (left, right, center)
         text - строка текста"""
 
-        # -- Свойства объекта
-        self.capt = capt  # Заголовок окна
-        self.pic = pic  # Файл картинки
-
-        if kwrd == 'center':
-            self.kwrd = k3.k_center
-        elif kwrd == 'right':
-            self.kwrd = k3.k_right
-        else:
-            self.kwrd = k3.k_left
-
-        self.text = text  # Текст в окне
+        self.Add_Caption(caption)
+        self.Add_Picture(picture)
+        self.Align_Text(align)
+        self.Add_Text(text)
 
         # -- Словарь из позиций меню
         self.content = {}
-
         # -- Упорядоченный список имён. В нём держим порядок позиций
         self.names = []
+
+    def Add_Caption(self, caption):
+        """Задаёт заголовок окна с меню"""
+
+        self.capt = caption
+
+    def Add_Picture(self, picture):
+        """Задаёт путь к файлу картинки"""
+
+        self.pic = picture
+
+    def Align_Text(self, align):
+        """Задаёт выравнивание текста"""
+
+        if align == 'right':
+            self.kwrd = k3.k_right
+        elif align == 'center':
+            self.kwrd = k3.k_center
+        else:
+            self.kwrd = k3.k_left
+
+    def Add_Text(self, text):
+        """Задаёт текст в окне меню"""
+
+        self.text = text
 
     def Show(self):
         """ При вызове отображает меню в k3 и при нажатии Ok заполняет словарь результатами"""
@@ -129,7 +152,7 @@ class k3menu:
         """ Добавляет разделительную линию """
         return self.Add_Simple('button', nam, '  ', 0)
 
-    def Add_Simple(self, type, nam='', pr='', d=''):
+    def Add_Simple(self, type, nam, pr, d):
         if not self.Can_Add(nam):
             return 0
         self.names.append(nam)
@@ -185,6 +208,13 @@ class k3menu:
             return 0
         return self.content[nam][1].value
 
+    def Val_From_Subst(self, name):
+        """Для позиций типа string 6 возвращает вторую половину значение, то есть id без подстановки"""
+        if self.Type(name) == 'string 6':
+            return self.Val(name).split('#')[1]
+        else:
+            return 0
+
     def Move(self, n1=0, n2=0):
         """Переносит номер n1 в позицию n2"""
         if n1 >= len(self.names) or n2 >= len(self.names):
@@ -207,30 +237,30 @@ class k3menu:
             return ''
         return self.names[n]
 
-    def Add_Real_List(self, nam='', pr='', l=(), d=0, only=0):
+    def Add_Real_List(self, nam='', pr='', contents=(), d=0, only=0):
         """Добавляет real с перечислением. d - значение умолчания для list или позиция current для only"""
         if not self.Can_Add(nam):
             return 0
-        if len(l) == 0:
+        if len(contents) == 0:
             return 0
         self.names.append(nam)
         if only == 0:
-            self.content[nam] = ['reallist', k3.Var(), d, pr, l]
+            self.content[nam] = ['reallist', k3.Var(), d, pr, contents]
         else:
-            self.content[nam] = ['reallistonly', k3.Var(), d, pr, l]
+            self.content[nam] = ['reallistonly', k3.Var(), d, pr, contents]
         return 1
 
-    def Add_String_List(self, nam='', pr='', l=(), d=0, only=0):
+    def Add_String_List(self, nam='', pr='', contents=(), d=0, only=0):
         """Добавляет string с перечислением. d - значение умолчания для list или позиция current для only"""
         if not self.Can_Add(nam):
             return 0
-        if len(l) == 0:
+        if len(contents) == 0:
             return 0
         self.names.append(nam)
         if only == 0:
-            self.content[nam] = ['stringlist', k3.Var(), d, pr, l]
+            self.content[nam] = ['stringlist', k3.Var(), d, pr, contents]
         else:
-            self.content[nam] = ['stringlistonly', k3.Var(), d, pr, l]
+            self.content[nam] = ['stringlistonly', k3.Var(), d, pr, contents]
         return 1
 
     def List(self, nam=''):
@@ -242,16 +272,16 @@ class k3menu:
             return []
         return self.content[nam][4]
 
-    def Setlist(self, nam='', l=()):
+    def Setlist(self, nam='', contents=()):
         """устанавливает позиции nam список l"""
         if not self.Is_Exist(nam):
             return 0
         possible = ['reallist', 'reallistonly', 'stringlist', 'stringlistonly']
         if possible.count(self.Type(nam)) == 0:
             return 0
-        if len(l) == 0:
+        if len(contents) == 0:
             return 0
-        self.content[nam][2] = l
+        self.content[nam][2] = contents
         return 1
 
     def Is_Exist(self, nam=''):
@@ -266,9 +296,7 @@ class k3menu:
         """Возвращает 1, если позицию можно добавить и 0, если nam пустое или такая позиция есть"""
         if nam == '':
             return 0
-        if self.names.count(nam) > 0:
-            return 0
-        return 1
+        return not self.Is_Exist(nam)
 
     def Curr(self, nam=''):
         """Возвращает значение текущего элемента списка для listonly"""
@@ -286,10 +314,10 @@ class k3menu:
         possible = ['reallistonly', 'stringlistonly']
         if possible.count(self.Type(nam)) == 0:
             return []
-        l = []
-        l.extend(self.List(nam))
-        l.insert(int(self.Def(nam)), k3.k_current)
-        return l
+        contents = []
+        contents.extend(self.List(nam))
+        contents.insert(int(self.Def(nam)), k3.k_current)
+        return contents
 
     def Refresh(self):
         """ Назначает всем умолчаниям текущие значение Надо добавить реакцию на only"""
@@ -299,8 +327,8 @@ class k3menu:
                     self.Setdef(self.names[i], self.Val(self.names[i]))
                 else:
                     val = self.Val(self.names[i])
-                    l = self.List(self.names[i])
-                    n = l.index(val)
+                    contents = self.List(self.names[i])
+                    n = contents.index(val)
                     self.Setdef(self.names[i], n)
 
     def Setdef(self, nam, d):
@@ -376,6 +404,155 @@ class k3picbox:
             self.text.append(text)
         elif isinstance(text, list) or isinstance(text, tuple):
             self.text.extend(text)
+
+
+class K3Menu2:
+    """Класс для меню К3 редакция 2.
+        Предполагает пункты меню как объекты отдельных классов"""
+
+    def __init__(self, caption='', picture='', align_keyword=k3.k_left):
+        self.caption = caption
+        self.picture = picture
+        self.align_keyword = align_keyword
+        self.text = ['']
+        self.items = []
+
+    def add_caption(self, caption):
+        """Задать заголовок окна"""
+        self.caption = caption
+
+    def add_picture(self, picture):
+        """Задать путь к файлу картинки"""
+        self.picture = picture
+
+    def align(self, align):
+        """Задать выравнивание текста 'left', 'right' или 'center' """
+        if align == 'center':
+            self.align_keyword = k3.k_center
+        elif align == 'right':
+            self.align_keyword = k3.k_right
+        else:
+            self.align_keyword = k3.k_left
+
+    def add_text(self, text):
+        """Добавить строку или множество строк к тексту"""
+        if len(self.text) == 1 and self.text[0] == '':
+            self.text = []
+        if isinstance(text, list or tuple):
+            self.text.extend(text)
+        elif isinstance(text, str):
+            self.text.append(text)
+
+    def clear_text(self):
+        """Очистить текст"""
+        self.text = ['']
+
+    def add_item(self, item):
+        """Добавить позицию (объект подкласса класса K3MenuItem)"""
+        if isinstance(item, K3MenuItem):
+            self.items.append(item)
+
+    def show(self):
+        """Отобразить меню, вернуть 1, если нажато Ok, или 0, если Отмена"""
+        setvar_parameters = [self.caption, self.picture, self.align_keyword]
+        setvar_parameters.extend(self.text)
+        setvar_parameters.append(k3.k_done)
+        for item in self.items:
+            if item(type):
+                setvar_parameters.extend(item.cmd_line())
+        setvar_parameters.append(k3.k_done)
+        res = k3.setvar(setvar_parameters)
+        return res[0]
+
+    def refresh(self):
+        """Подставить в умолчание каждому пункту его текущее значение"""
+        for item in self.items:
+            item.refresh()
+
+
+class K3MenuItem:
+    """Родительский класс для пункта меню К3"""
+    types = ('real', 'real_list', 'real_list_on')
+
+    def __init__(self):
+        self.prompt = ''
+        self.type = ''
+        self.default = 0
+        self.size = 0
+
+    def refresh(self):
+        """Подставляет в умолчание текущее значение"""
+        self.default = self.value()
+
+    def value(self):
+        """Возвращает текущее значение"""
+        pass
+
+    def cmd_line(self):
+        """Возвращает подстановку в командную строку для вызова setvar"""
+        pass
+
+    def set_default(self, default):
+        self.default = default
+
+    def set_prompt(self, prompt):
+        self.prompt = prompt
+
+    def set_size(self, size):
+        self.size = size
+
+
+class ItemSimple(K3MenuItem):
+    """Простые вводы в строку с возвратом одного значения"""
+
+    def __init__(self, prompt='', default=0, size=0):
+        super().__init__()
+        self.set_size(size)
+        self.set_default(default)
+        self.set_prompt(prompt)
+        self.type = ''
+        self.k3var = k3.Var()
+        self.k3var.value = self.default
+
+    def value(self):
+        return self.k3var.value
+
+    def cmd_line(self):
+        cmd_line = [self.type]
+        if not self.type == 'logical':
+            if self.size == 0:
+                cmd_line.append(k3.k_auto)
+            elif self.size > 0:
+                cmd_line.extend([k3.k_size, int(self.size)])
+        cmd_line.extend([k3.k_default, self.default])
+        cmd_line.append(self.prompt)
+        cmd_line.append(self.k3var)
+
+
+class ItemReal(ItemSimple):
+    """Ввод числа"""
+
+    def __init__(self, prompt='', default=0, size=0):
+        super().__init__(prompt=prompt, default=default, size=size)
+        self.type = 'real'
+
+
+class ItemString(ItemSimple):
+    """Ввод Строки"""
+
+    def __init__(self, prompt='', default='', size=0):
+        super().__init__(prompt=prompt, default=default, size=size)
+        self.type = 'string'
+
+
+class ItemLogical(ItemSimple):
+    """Чекбокс"""
+    def __init__(self, prompt='', default=0, size=0):
+        super().__init__(prompt=prompt, default=default, size=size)
+        self.type = 'logical'
+
+
+# class ItemFile
 
 
 if __name__ == '__main__':
